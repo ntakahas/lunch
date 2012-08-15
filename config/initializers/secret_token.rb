226b548dc24e370d3b200344d2ed1e408fe0545d
TAKAHASHI Naoto <naoto.takahashi@aist.go.jp>
2012-08-15 17:33:00 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunch::Application.config.secret_token = 'de0cadfd21a1caccff7aaa1473d53c03b0aabab91e2edbdb0beaabd3296e6ecee67fcee309ddcee94b718e5558ddc62de67a6db41cea09aff0c7d8f31c076b5c'
